3f7c18434fe23e85564641c450a4bd5b68fb2139
Meziane Dahou <hd_meziane@esi.dz>
2020-12-11 18:17:51 +0100

    first commit

---
+++ IOS Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Calc
+//
+//  Created by Dahou Meziane on 12/11/20.
+//  Copyright © 2020 Meziane Dahou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
